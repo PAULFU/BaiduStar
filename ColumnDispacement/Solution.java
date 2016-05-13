c0397fc5582a24c0c1cce8fc06d23b646fecf095
fupinyou <paulfun001@gmail.com>
2016-05-13 18:41:27 +0800

    1002

--- ColumnDispacement/Solution.java
+++ ColumnDispacement/Solution.java
@@ -18,8 +18,7 @@ public class Solution{
                 mark = mark % key + 1;   //每列的余数是相同的，加1表示向右移动一列
             }
         }
-        String rst = new String(result);
-        return rst;
+        return new String(result);
     }
 
     public static void main(String[] args) {
