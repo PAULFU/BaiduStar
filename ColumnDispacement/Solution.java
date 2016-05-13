c86c6966f2ce2d42c529aaec472f83d713c7376b
pinyoufu <pinyoufu@outlook.com>
2016-05-13 23:37:47 +0800

    原来问题一直出在nextInt()这个方法

--- ColumnDispacement/Solution.java
+++ ColumnDispacement/Solution.java
@@ -10,12 +10,13 @@ public class Solution{
     public static String decryption(int key, String string) {
         int length = string.length();
         char[] ch = string.toCharArray();
+        //System.out.println(ch.length);
         char[] result = new char[length];
         int mark = 0;
         for (int i = 0; i < length; i++) {
             result[mark] = ch[i];
             mark = mark + key;
-            if (mark >= length) {        //大于时，说明这一列已经读完了，此时需要换一列
+            if (mark >= length) {        //大于时，说明这一列已经遍历完了，此时需要换一列
                 mark = mark % key + 1;   //每列的余数是相同的，加1表示向右移动一列
             }
         }
@@ -26,16 +27,16 @@ public class Solution{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int count = sc.nextInt();
+        sc.nextLine();
         String[] st=new String[count];
         int[] k=new int[count];
         for (int i = 0; i < count; i++) {
-            st[i] = sc.next();
+            st[i] = sc.nextLine();
             k[i] = sc.nextInt();
+            sc.nextLine();
         }
-
-
         for (int j=0;j<count;j++){
-            System.out.println("Case #" + (j + 1));
+            System.out.println("Case #" + (j + 1)+":");
             System.out.println(decryption(k[j], st[j]));
         }
     }
